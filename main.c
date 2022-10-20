d00b91869bcd0c9f18c9dd6d72df1f085b0bf1fc
MoffitTheCEO <71614354+MoffitTheCEO@users.noreply.github.com>
2022-10-19 21:31:24 -0500

    First Data Capture implemented (When Normalization is needed) *Untested

--- main.c
+++ main.c
@@ -10,13 +10,12 @@ char DMA_UART_TX_BUFFER[BUFFER_SIZE];
 #INT_DMA0
 void  DMA_0_ISR(void) 
 {
-   DMA_DONE = 1;
+   DMADoneFlag = 1;
 }
 
 #INT_DMA1
 void DMA_1_ISR(void)
 {
-
 }
 
 #INT_TIMER1
@@ -25,43 +24,168 @@ void Timer_ISR()
    read_adc();
 }
 
+#INT_RDA2      // interrupt handler for the uart RS232 communication
+void ISR_UART2()
+{
+    UARTRX = fgetc(SHARP); // recieve data
+    UARTRXFlag = 1;         // serial flag turned high
+}
+
 void main()
 {   
    memset(DMA_ADC_BUFFER, 0, BUFFER_SIZE);
    memset(DMA_UART_TX_BUFFER, 'a', BUFFER_SIZE);
    
-   setup_dma(ADC_DMA_CHANNEL, DMA_IN_ADC1, DMA_WORD);
+   setup_dma(ADC_DMA_CHANNEL, DMA_IN_ADC1, DMA_BYTE);
    dma_start(ADC_DMA_CHANNEL, DMA_CONTINOUS, &DMA_ADC_BUFFER[0], BUFFER_SIZE);
-
    
-   setup_dma(UART_TX_DMA_CHANNEL, DMA_OUT_UART2, DMA_BYTE | DMA_HALF_INT);
+   setup_dma(UART_TX_DMA_CHANNEL, DMA_OUT_UART2, DMA_BYTE);
 
    enable_interrupts(INT_DMA0);
    enable_interrupts(INT_DMA1);
    
    setup_adc_ports(sAN0, VSS_VDD);
-   setup_adc(ADC_CLOCK_INTERNAL | ADC_TAD_MUL_4);
+   setup_adc(ADC_CLOCK_INTERNAL);
    
    read_adc();
    
-   setup_timer1(TMR_INTERNAL ,60000);
+   setup_timer1(TMR_INTERNAL ,10000);
    enable_interrupts(INT_TIMER1);
    enable_interrupts(INTR_GLOBAL);
 
    while(TRUE)
    {
-      delay_ms(1000);
-      if(DMA_DONE)
+      
+      if(DMADoneFlag)
       {
          disable_interrupts(INT_DMA0);
-         for(unsigned int16 i=0; i< BUFFER_SIZE; i++)
+//!         for(unsigned int16 i=0; i< BUFFER_SIZE; i++)
+//!         {
+//!               printf("\n\r ADC Val: %c", DMA_ADC_BUFFER[i]);
+//!         }
+         for (IndexType Index = 0; Index < NumberOfDigitizationRequired; Index++)
          {
-               printf("\n\r ADC Val: %d", DMA_ADC_BUFFER[i]);
+            AccumulateAnalogData(Index);
+         }
+         
+         
+         if (NormalizeFlag == 1)
+         {
+            
+            NormalizeData();
+         }
+         else
+         {
+         
          }
          
          dma_start(UART_TX_DMA_CHANNEL, DMA_ONE_SHOT | DMA_FORCE_NOW, &DMA_ADC_BUFFER[0], BUFFER_SIZE);
          enable_interrupts(INT_DMA0);
-         DMA_DONE = 0;
+         DMADoneFlag = 0;
       }      
    }
+}
+
+void AccumulateAnalogData(IndexType NumberOfDigitizationRequired)
+{
+   IndexType DMAADCIndex = (NumberOfDigitizationRequired * COEF_LENGTH);
+   
+   for (IndexType Index = 0; Index < COEF_LENGTH; Index++) //Todo:: Replace with MemCpy()
+   {
+      InputSamples[Index] = DMA_ADC_BUFFER[DMAADCIndex++];
+   }
+   
+   DMAADCIndex = (NumberOfDigitizationRequired * COEF_LENGTH);
+   InputIndex = CurrentIndex; 
+   Accumulator = 0;
+   CoefficentIndex = 0;
+   
+   for (IndexType Index = 0; Index < COEF_LENGTH; Index++)
+   {
+      while (CoefficentIndex < COEF_LENGTH - 1)
+      {
+         Accumulator += (signed int32)InputSamples[InputIndex] * (signed int32)fir_coef[CoefficentIndex];
+           // condition for the circular buffer
+         if (InputIndex == COEF_LENGTH - 1)
+         {
+            InputIndex = 0;
+         }
+         else
+         {
+            InputIndex++;
+         }
+         CoefficentIndex++;
+      }
+      
+      AnalogData[DMAADCIndex++] = InputSamples[Index];
+      
+      if (NormalizeFlag == 1)
+      {
+         DigitizedData[DMAADCIndex] = Accumulator;
+      }
+      else
+      {
+         OutputValue = (Accumulator - AverageAnalogValue) * AverageMultiplier + (ADC_MAX_DATA_VALUE / 2);
+         ConversionValue = (unsigned int8)OutputValue;
+         DigitizedData[DMAADCIndex] = ConversionValue;
+      }
+   }
+  
+}
+
+void NormalizeData()
+{
+   
+   if (ErrorCounter < 3000)
+   {
+      MaxAnalogValue = DigitizedData[64]; //Todo::Remove Gloab Vairables Where Possbile 
+      MinAnalogValue = DigitizedData[64]; //Todo::Remove Gloab Vairables Where Possbile 
+      InitialTriggerValue = DMA_ADC_BUFFER[64];
+      
+      for (IndexType Index = 64; Index < BUFFER_SIZE; Index++)
+      {
+         if (MinAnalogValue > DigitizedData[Index])
+         {
+            MinAnalogValue = DigitizedData[Index];
+         }
+         
+         if (MaxAnalogValue < DigitizedData[Index])
+         {
+            MaxAnalogValue = DigitizedData[Index];
+         }
+         
+         if (InitialTriggerValue > DMA_ADC_BUFFER[Index])
+         {
+            InitialTriggerValue = DMA_ADC_BUFFER[Index];
+         }
+         
+         AverageAnalogValue = AverageAnalogValue + DigitizedData[Index];
+      }
+      
+      AverageDivider = MaxAnalogValue - MinAnalogValue;
+      AverageMultiplier = ((float)ADC_MAX_DATA_VALUE / AverageDivider);
+      AverageAnalogValue = AverageAnalogValue / (137);
+      
+      TriggerValue = InitialTriggerValue;
+               
+      memset(DigitizedData, 0, BUFFER_SIZE);
+      memset(DMA_ADC_BUFFER, 0, BUFFER_SIZE);           
+   }  
+   else 
+   {
+      InitialTriggerValue = DMA_ADC_BUFFER[64];
+      for (int i = 65; i < BUFFER_SIZE; i++)
+      {
+        
+        if (InitialTriggerValue > DMA_ADC_BUFFER[i])
+        {
+            InitialTriggerValue = DMA_ADC_BUFFER[i];
+        }
+        
+        TriggerValue = InitialTriggerValue;
+        ErrorCounter = 0; 
+      }
+   }
+   
+   CurrentIndex = 0;
 }
--- main.h
+++ main.h
@@ -22,12 +22,65 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <ctype.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 256
+#define COEF_LENGTH 64
+#define LOADER_PAGES 50
+#define ADC_MAX_DATA_VALUE 255
 
-unsigned int8 DMA_DONE = 0;
+typedef unsigned int32 IndexType;
+typedef unsigned int8 FlagType;
+
 const unsigned int8 ADC_DMA_CHANNEL = 0;
 const unsigned int8 UART_TX_DMA_CHANNEL = 1;
+const IndexType NumberOfDigitizationRequired = BUFFER_SIZE / COEF_LENGTH;
+
+unsigned int16 Frequency = 0;
+
+unsigned int8 ConversionValue; // normalized value
+unsigned int8 AnalogData[BUFFER_SIZE]; // input array
+
+unsigned int8 CSharpCoefficent[2];          // array to hold byte data of coefficents
+
+unsigned int16 InitialTriggerValue = 0; // used for initail trigger value
+unsigned int16 TriggerValue = 0; // used for initail trigger value
+
+signed int16 ByteConversionResult = 0;     // result of byte convertion
+
+signed int32 Accumulator = 0; // accumulator of the output value in the difference equation calculation
+signed int32 DigitizedData[BUFFER_SIZE];   // output array
+signed int32 MaxAnalogValue = 0; // use for normalization
+signed int32 MinAnalogValue = 0; // use for normalization
+
+signed int64 AverageAnalogValue = 0; // use for normalization
+
+signed int16  InputSamples[COEF_LENGTH]; // array used as a circular buffer for the input samples
+
+float OutputValue;          // holds the current output value
+float AverageDivider = 0; // use for normalization
+float AverageMultiplier = 0;    // use for normalization
+
+char UARTRX; // data in
+
+IndexType CSharpCoefficentRecieved = 0;    // counter of coefficents recieved from c#
+IndexType CoefficentIndex = 0;            // used as the index for the filter coefficients array in the difference equation calculation
+IndexType InputIndex= 0;           // used as the index for the input samples array in the difference equation calculation
+IndexType CurrentIndex = 0;          // keeps track of the current position of the circular buffer
+IndexType NumberCSharpByteRecieved = 0; // counter to track which byte is being recieved
+IndexType ErrorCounter = 0; // error detection
+
+FlagType DMADoneFlag = 0;
+FlagType NormalizeFlag = 0;
+
+FlagType UARTRXFlag = 0;   // serial flag
+FlagType UARTTXFlag = 0;       // flag to send data out
+FlagType HandShakeFlag = 1;    // handshake
+
+void AccumulateAnalogData(IndexType);
+void NormalizeData();
+
+signed int16 fir_coef[COEF_LENGTH] = {  };
 
 #endif // MAIN_H
 
